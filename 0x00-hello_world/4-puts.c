68e632b30786778113f0bdf59490a5eb90c9be8d
abelbr1 <abelbrhanuab@gmaiil.com>
2022-04-08 04:25:37 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-     puts("\"programming is like building a multilingual puzzle);
+     puts("\"programming is like building a multilingual puzzle");
      return (0);
 }
