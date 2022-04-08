35db9c36da809e7126216822ad1b37dc64c79496
abelbr1 <abelbrhanuab@gmail.com>
2022-04-08 03:54:08 -0700

    All Done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	char c;
+	char c,b;
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-
+        for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
 	putchar('\n');
 	return (0);
 }
