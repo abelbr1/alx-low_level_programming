231ae2519543ec7e5ac3ad8b2381bdd7528035db
abelbr1 <abelbrhanuab@gmail.com>
2022-04-29 04:46:24 -0700

    argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,23 +6,14 @@
  * @argc: Count arguments
  * @argv: Arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0 - Success.
  */
 
 int main(int argc, char *argv[])
 {
+	int i;
 
-/*Declaring variables*/	
-int count = 0;
-
-if (argc > 0)
-{
-/*WHILE - Print each arguments*/
-while (count < argc)
-{
-printf("%s\n", argv[count]);
-count++;
-}
-}
-return (0);
+	for(i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
