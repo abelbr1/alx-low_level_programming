9d27bb422f5ee00288d4d56f741e8c6c48b70616
abelbr1 <abelbrhanuab@gmail.com>
2022-07-15 21:32:47 +0300

    creating...

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -fPIC *.c
-gcc -shared -o libdynamic.so *.o
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libdynamic.so
-LD_PRELOAD=$WPD/libdynamic.so
+wget -P /tmp https://github.com/abelbr1/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
